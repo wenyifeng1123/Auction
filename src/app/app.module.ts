475ffe59b3e810dcd69acf47a933e94012bcabfe
wenyifeng1123 <33321977+wenyifeng1123@users.noreply.github.com>
2018-02-26 23:36:07 +0100

    add the route module del import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import {AppRoutingModule} from '.app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,7 +12,7 @@ import { StarsComponent } from './stars/stars.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 
 const routeConfig: Routes = [
   {path: '', component: HomeComponent},
