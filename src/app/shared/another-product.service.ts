a4c09e49c624771042d9289266b90faef243edf5
wenyifeng1123 <124392170@qq.com>
2018-02-27 21:49:31 +0100

    2 services

---
+++ src/app/shared/another-product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Product, ProductService} from './product.service';
+
+@Injectable()
+export class AnotherProductService implements ProductService{
+
+  getProduct(): Product {
+    return new Product(1, "sumsung7", 4899, "lastest sumsung");
+  }
+  constructor() { }
+
+}
